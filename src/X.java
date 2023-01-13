c8967fb9d04ada1d95530bcbc7ef66ba2a6512eb
faisal <mohammadfaisalk10@gmail.com>
2023-01-13 19:49:04 +0530

    third

--- src/X.java
+++ src/X.java
@@ -4,5 +4,11 @@ public static void main(String[] args) {
     System.out.println("x"); 
     System.out.println("z"); 
     System.out.println("y");
+    
+    System.out.println("1");
+    System.out.println("2");
+    System.out.println("3");
+    
+    
 }
 }
